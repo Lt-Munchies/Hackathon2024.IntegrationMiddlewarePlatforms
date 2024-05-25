b24340cccbc0d46d586c3b46eb1f418e9f566bbe
Reon Fourie <reonfourie1@gmail.com>
2024-05-25 14:53:03 +0200

    trying hard

--- src/Hackathorn2024/Hackathorn2024.Server/Models/PromptResponse.cs
+++ src/Hackathorn2024/Hackathorn2024.Server/Models/PromptResponse.cs
@@ -1,14 +1,30 @@
-﻿namespace Hackathorn2024.Server.Models
+﻿using Newtonsoft.Json;
+
+namespace Hackathorn2024.Server.Models
 {
     public class PromptResponse
     {
+        [JsonProperty("Business Name")]
         public string BusinessName { get; set; }
+        [JsonProperty("Bank Name")]
         public string BankName { get; set; }
+        [JsonProperty("Branch Name")]
         public string BranchName { get; set; }
+        [JsonProperty("Branch Number")]
         public string BranchNumber { get; set; }
+        [JsonProperty("Account Number")]
         public string AccountNumber { get; set; }
+        [JsonProperty("Account Name")]
         public string AccountName { get; set; }
+        [JsonProperty("Payment Reference")]
         public string PaymentReference { get; set; }
+        [JsonProperty("Invoice Total")]
         public string InvoiceTotal { get; set; }
+    }
+
+    public class Content
+    {
+        [JsonProperty("content")]
+        public PromptResponse response { get; set; }
     }
 }
