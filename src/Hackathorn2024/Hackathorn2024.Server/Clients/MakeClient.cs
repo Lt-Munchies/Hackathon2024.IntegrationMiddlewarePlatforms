75f5827a6cea91cd2ddff1e3c56e79312d057145
Christian Smith <Christian.Smith@derivco.co.za>
2024-05-25 15:38:10 +0200

    push

---
+++ src/Hackathorn2024/Hackathorn2024.Server/Clients/MakeClient.cs
@@ -0,0 +1,27 @@
+﻿// using Flurl;
+// using Flurl.Http;
+//
+// namespace Hackathorn2024.Server.Clients;
+//
+// public class MakeClient(ILogger<BaseHttpClient> logger, HttpClient client) : BaseHttpClient(logger, client)
+// {
+//     public async Task PostFileStringAsync(MakeFileRequest request, CancellationToken token = default)
+//     {
+//         return await WrapRetryRequestAsync(async client =>
+//         {
+//             var request = client.WithHeader("Content-Type", "application/json")
+//                 .Request();
+//
+//             request.Url = new Url("https://api.make.com")
+//                 .AppendPathSegment("v1")
+//                 .AppendPathSegment("make");
+//
+//             await request.PostAsync(cancellationToken: token);
+//         });
+//     }
+// }
+//
+// public class MakeFileRequest
+// {
+//
+// }
